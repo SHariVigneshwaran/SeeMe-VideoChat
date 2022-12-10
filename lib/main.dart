624a8638ba40d19957d59b8f5e0f38ed8e631bb6
SHariVigneshwaran <aiharivigneshwaran@gmai.com>
2022-12-11 00:05:55 +0530

    1st commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:see_me/resources/auth_methods.dart';
+import 'package:see_me/screens/home_screen.dart';
+import 'package:see_me/screens/login_screen.dart';
+import 'package:see_me/screens/video_call_screen.dart';
+import 'package:see_me/utils/colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'See Me',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/home': (context) => const HomeScreen(),
+        '/video-call': (context) => const VideoCallScreen(),
+      },
+      home: StreamBuilder(
+        stream: AuthMethods().authChanges,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          }
+
+          return const LoginScreen();
+        },
+      ),
+    );
+  }
+}
